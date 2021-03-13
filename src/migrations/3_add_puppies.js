5a647e0db008fa898c1f3351af7e64090cc42b42
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-03-13 14:01:38 +1000

    Chore:  Update migrate script

--- src/migrations/3_add_puppies.js
+++ src/migrations/3_add_puppies.js
@@ -5,17 +5,12 @@ const neatCsv = require("neat-csv");
 
 const DogERC721 = artifacts.require("DogERC721");
 
-module.exports = function (deployer) {
+module.exports = async function (deployer) {
     const dogs = await DogERC721.deployed();
-    const filePath = path.join(__dirname, "beagles.csv");
 
-    fs.readFile(filePath, async (error, data) => {
-        if (error) {
-            return console.log("error reading file");
-        }
-        
-        const parsedData = await neatCsv(data);
-        console.log(parsedData);
-        //await dogs.addPuppy(datum[0], datum[1], datum[2], datum[3], datum[4], datum[5], datum[6]);
-    });
+    const dob = new Date("2019-02-07");
+    const epoch = dob.getTime();
+    // string calldata name, uint256 dob, bytes32 microchip, Sex sex, uint256 dam, uint256 sire, address owner
+    // BALDEV QUEEN OF DIAMONDS,2019-02-07
+    await dogs.addPuppy("BALDEV QUEEN OF DIAMONDS", epoch, "0x956000010822866", 0, 0, 0, "0xf51f726449bf2944f11652A349655Bd53c48c9A6");
   };
--- src/truffle.js
+++ src/truffle.js
@@ -21,7 +21,7 @@ module.exports = {
     development: {
       host: "localhost", // Localhost (default: none)
       port: 8545, // Standard Ethereum port (default: none)
-      network_id: "5777" // Any network (default: none)
+      network_id: "*" // Any network (default: none)
     }
   },
   // Set default mocha options here, use special reporters etc.
