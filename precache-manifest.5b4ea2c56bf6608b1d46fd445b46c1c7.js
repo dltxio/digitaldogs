976310e33e2b2b96db8fd08922b62b151358c2af
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-03-18 11:34:51 +1000

    Updates

---
+++ precache-manifest.5b4ea2c56bf6608b1d46fd445b46c1c7.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aabafbe97b8e3919603c82a4bd54386b",
+    "url": "https://www.dltx.io/digitaldogs.github.io/index.html"
+  },
+  {
+    "revision": "bdd06d936eb63026c2a2",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/css/2.e2d9960b.chunk.css"
+  },
+  {
+    "revision": "3e9e4034526803f6bf44",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/css/main.7076e224.chunk.css"
+  },
+  {
+    "revision": "bdd06d936eb63026c2a2",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/js/2.bc57e2de.chunk.js"
+  },
+  {
+    "revision": "6d4d13f96b2f3090827d8bd03aa15199",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/js/2.bc57e2de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e9e4034526803f6bf44",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/js/main.0a44ce1c.chunk.js"
+  },
+  {
+    "revision": "b238b2a9a56d2c69d5f9",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/js/runtime-main.1639b932.js"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/Simple-Line-Icons.0cb0b9c5.woff2"
+  },
+  {
+    "revision": "2fe2efe63441d830b1acd106c1fe8734",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/Simple-Line-Icons.2fe2efe6.svg"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/Simple-Line-Icons.78f07e2c.woff"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/Simple-Line-Icons.d2285965.ttf"
+  },
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/Simple-Line-Icons.f33df365.eot"
+  },
+  {
+    "revision": "31f926ac134297d62f4ef885ff83c526",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/bg-callout.31f926ac.jpg"
+  },
+  {
+    "revision": "220f93149fc37933676a865b3ce5e2d6",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/bg-masthead.220f9314.jpg"
+  },
+  {
+    "revision": "48461ea4e797c9774dabb4a0440d2f56",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/fa-brands-400.48461ea4.woff2"
+  },
+  {
+    "revision": "7b464e274bc331f9a765d765359635a5",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/fa-brands-400.7b464e27.woff"
+  },
+  {
+    "revision": "947b9537bc0fecc8130d48eb753495a1",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/fa-brands-400.947b9537.ttf"
+  },
+  {
+    "revision": "9b6c8da3c489424e2b3e9c9fb6314b37",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/fa-brands-400.9b6c8da3.eot"
+  },
+  {
+    "revision": "b5472631dbace30d549357ec325b9c62",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/fa-brands-400.b5472631.svg"
+  },
+  {
+    "revision": "381af09a1366b6c2ae65eac5dd6f0588",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/fa-regular-400.381af09a.woff"
+  },
+  {
+    "revision": "73fe7f1effbf382f499831a0a9f18626",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/fa-regular-400.73fe7f1e.ttf"
+  },
+  {
+    "revision": "7422060ca379ee9939d3b687d072acad",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/fa-regular-400.7422060c.eot"
+  },
+  {
+    "revision": "949a2b066ec37f5a384712fc7beaf2f1",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/fa-regular-400.949a2b06.woff2"
+  },
+  {
+    "revision": "b5a61b229c9c92a6ac21f5b0e3c6e9f1",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/fa-regular-400.b5a61b22.svg"
+  },
+  {
+    "revision": "0079a0ab6bec4da7d6e16f2a2e87cd71",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/fa-solid-900.0079a0ab.ttf"
+  },
+  {
+    "revision": "14a08198ec7d1eb96d515362293fed36",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/fa-solid-900.14a08198.woff2"
+  },
+  {
+    "revision": "38508b2e7fac045869a86a15936433f7",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/fa-solid-900.38508b2e.svg"
+  },
+  {
+    "revision": "70e65a7d34902f2c350816ecfe2f6492",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/fa-solid-900.70e65a7d.eot"
+  },
+  {
+    "revision": "815694de1120d6c1e9d1f0895ee81056",
+    "url": "https://www.dltx.io/digitaldogs.github.io/static/media/fa-solid-900.815694de.woff"
+  }
+]);
